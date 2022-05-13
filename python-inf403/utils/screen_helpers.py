591bd182e4827aa4b2d2fe82b32b467d3f7cbb55
zeynel95 <zeynelaltun95@gmail.com>
2022-05-13 11:27:07 +0200

    screen helpers modification

--- python-inf403/utils/screen_helpers.py
+++ python-inf403/utils/screen_helpers.py
@@ -7,17 +7,23 @@ def print_introduction():
     
 def print_description():
     print("--------Description-------------")
+    print("Vous etes sur une page d'achat et de vente d'audio")
+    print("Pour acheter ou vendre il faut se connecter")
 
     
 def print_help():
     print("--------Help-------------")
-
+    print("les commandes possibles sont :")
+    print("[-] --description")
+    print("[-] --connexion")
+    print("[-] --information")
     
 def print_information():
     print("--------Information-------------")
+    print("[-] clients")
+    print("[-] auteurs")
 
-def print_falseCommand():
-    print("Command not recognized!")
-
-def print_availableCommand(current_state):
-    print("Commands available: ", states_commands[current_state])
+def print_connexion():
+    print("---------Connexion----------")
+    id = input("entrez votre id")
+    return id
